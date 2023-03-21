4eeb13a42cb6536a7feed0e0773f9a7e03e60478
Matt Wylder <matt@wylder.us>
2023-03-21 14:14:44 -0500

    Initial Commit

---
+++ Exposure/ExposureApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExposureApp.swift
+//  Exposure
+//
+//  Created by Matthew Wylder on 3/21/23.
+//
+
+import SwiftUI
+
+@main
+struct ExposureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
